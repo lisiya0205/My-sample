82a2655a453d756ecc65c5492f9ac65b290b0997
lisiya0205 <lisiyarithik@gmail.com>
2025-04-18 21:44:43 +0530

    Update A.java

--- src/practice/A.java
+++ src/practice/A.java
@@ -4,7 +4,8 @@ public class A {
 
 	public static void main(String[] args) {
 		int a=10,b=20;
-		System.out.println("The value of a is "+a);
+		System.out.println("Hi");
+		System.out.println(" The value of a is "+a);
 		System.out.println("The value of b is  "+b);
 		a=a+b;
 		b=a-b;
